eae794d5d6ce9ec9f61893490c8bab7a35d577d1
algerza <alvaro.ager@gmail.com>
2023-06-18 20:58:14 +0300

    update functions, libraries, dataset

--- utils.py
+++ utils.py
@@ -1,28 +1,193 @@
 from dateutil import parser
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-# def convert_dates_to_iso(df):
-#     """
-#     A function to convert all date columns in a DataFrame to ISO 8601 format.
+
+
+def convert_dates_to_iso(df):
+    """
+    A function to convert all date columns in a DataFrame to ISO 8601 format.
     
-#     Parameters:
-#     df: a pandas DataFrame
+    Parameters:
+    df: a pandas DataFrame
     
-#     Returns:
-#     df: the updated DataFrame with date columns in ISO 8601 format
-#     """
-#     # Loop through all columns in the DataFrame
-#     for col in df.columns:
-#         # Check if the column data type is object
-#         if df[col].dtype == 'object':
-#             # Try to parse the string date values in the column and convert to ISO 8601 format
-#             try:
-#                 df[col] = df[col].apply(lambda x: parser.parse(x).strftime('%Y-%m-%d') if isinstance(x, str) else x)
-#             except ValueError:
-#                 # If there's an error parsing the string date values, skip the column and move to the next one
-#                 pass
-#     # Return the updated DataFrame
-#     return df
+    Returns:
+    df: the updated DataFrame with date columns in ISO 8601 format
+    """
+    # Loop through all columns in the DataFrame
+    for col in df.columns:
+        # Check if the column data type is object
+        if df[col].dtype == 'object':
+            # Try to parse the string date values in the column and convert to ISO 8601 format
+            try:
+                df[col] = df[col].apply(lambda x: parser.parse(x).strftime('%Y-%m-%d') if isinstance(x, str) else x)
+            except ValueError:
+                # If there's an error parsing the string date values, skip the column and move to the next one
+                pass
+    # Return the updated DataFrame
+    return df
+
+
+def check_for_duplicates(df):
+    """Checks for duplicate values in a given dataframe."""
+    if df.duplicated().any().any():
+        # if any duplicated values exist, print a message indicating their presence
+        print("- There are duplicated values in the dataframe.")
+    else:
+        # if no duplicated values exist, print a message indicating their absence
+        print("- There are no duplicated values in the dataframe.")
+
+
+def check_for_missing_values(df):
+    """Checks for missing values in a given dataframe."""
+    if df.isnull().any().any():
+        # create a list of columns with missing values
+        null_cols = df.columns[df.isnull().any()].tolist()
+        for col in null_cols:
+            # for each column with missing values, print a message indicating their presence
+            print(f"- There are missing values in the dataframe. Column '{col}' has missing values.")
+    else:
+        # if no missing values exist, print a message indicating their absence
+        print("- There are no missing values in the dataframe.")
+
+
+def check_for_imbalanced_dataset(df):
+    """Checks for class imbalance in a given dataframe."""
+    # count the number of samples in each class
+    class_counts = df['Churn'].value_counts()
+
+    # calculate the imbalance ratio
+    imbalance_ratio = class_counts.min() / class_counts.sum()
+
+    if imbalance_ratio < 0.3:
+        # if the imbalance ratio is below a threshold, print a message indicating class imbalance
+        print(f"- The dataframe is imbalanced, with an imbalance ratio of {round(imbalance_ratio, 2)}.")
+    else:
+        # if the imbalance ratio is above a threshold, print a message indicating class balance
+        print(f"- The dataframe is balanced, with an imbalance ratio of {round(imbalance_ratio, 2)}.")
+
+
+def flag_outliers(df):
+    """
+    This function takes a pandas DataFrame as input and flags outliers in numerical columns.
+    """
+    # Create a new DataFrame to store the outlier flags
+    outlier_flags = pd.DataFrame(index=df.index)
+
+    # Loop through each numerical column in the input DataFrame
+    for col in df.select_dtypes(include=np.number):
+        # Skip outlier detection for binary columns
+        if df[col].nunique() == 2:
+            continue
+        
+        # Calculate the mean and standard deviation of the column
+        col_mean = df[col].mean()
+        col_std = df[col].std()
+
+        # Calculate the 1st and 99th percentiles of the column
+        col_1st_perc = np.percentile(df[col], 1)
+        col_99th_perc = np.percentile(df[col], 99)
+
+        # Calculate the lower and upper bounds for outlier detection
+        lower_bound = col_mean - 3 * col_std
+        upper_bound = col_mean + 3 * col_std
+
+        # Determine if there are any outliers in the column
+        has_outliers = ((df[col] < col_1st_perc) | (df[col] > col_99th_perc) | 
+                        (df[col] < lower_bound) | (df[col] > upper_bound)).any()
+
+        # If there are outliers, create a new column in the outlier flags DataFrame
+        # that flags whether or not each value in the input column is an outlier
+        if has_outliers:
+            outlier_flags[f"{col}_outlier"] = (
+                (df[col] < col_1st_perc) | (df[col] > col_99th_perc) | 
+                (df[col] < lower_bound) | (df[col] > upper_bound)
+            ).astype(int)
+
+            print("Outlier column created:", col)
+            
+        else: print("No outlier columns created")
+
+    # Join the outlier flags DataFrame with the input DataFrame
+    df = pd.concat([df, outlier_flags], axis=1)
+
+    # Return the new DataFrame with outlier flags
+    return df
+
+
+def clamp_outliers(df):
+    """
+    Replace values in numerical columns that are more than 3 standard deviations from the mean,
+    higher than the 99th percentile or lower than the 1st percentile with the nearest value that is in range.
+
+    Args:
+    df: pandas DataFrame object.
+
+    Returns:
+    pandas DataFrame object with outliers clamped.
+    """
+    print("Columns to be modified: ")
+    for col in df.select_dtypes(include=[np.number]):
+        if len(df[col].unique()) == 2:
+            continue
+        lower_bound = df[col].quantile(0.01)
+        upper_bound = df[col].quantile(0.99)
+        std_dev = df[col].std()
+        outlier_mask = (df[col] < (df[col].mean() - (3 * std_dev))) | (df[col] > (df[col].mean() + (3 * std_dev))) | (df[col] < lower_bound) | (df[col] > upper_bound)
+        num_outliers = len(df[outlier_mask].index)
+        if num_outliers > 0:
+            print(col)
+            df[col] = np.clip(df[col], lower_bound, upper_bound)
+    print(f"\n{num_outliers} customer IDs affected.")
+    return df
+
+
+
+def plot_churn_below_above_avg(data, column_name):
+    """
+    Plots target column's churn rate for the segment above and below average
+      compared to the overall churn rate
+
+    Args:
+    pandas DataFrame column name.
+
+    Returns:
+    bar plot.
+    """
+
+    churn_rate_overall = round(data[data['Churn'] == 1].customerID.nunique() / data.customerID.nunique(), 2)
+
+    monthly_mean = data[column_name].mean()
+
+    less_avg = data[(data[column_name] < monthly_mean)].shape[0]
+    churned_less_avg = data[(data[column_name] < monthly_mean) & (data['Churn'] == 1)].shape[0]
+    churn_rate_less_avg = round(churned_less_avg/less_avg,2)
+
+    more_avg = data[(data[column_name] > monthly_mean)].shape[0]
+    churned_more_avg = data[(data[column_name] > monthly_mean) & (data['Churn'] == 1)].shape[0]
+    churn_rate_more_avg = round(churned_more_avg/more_avg,2)
+
+
+    churn_plot_df = pd.DataFrame({
+            'segment': [churn_rate_less_avg, churn_rate_overall, churn_rate_more_avg],
+            'churn': ['churn_rate_less_avg', 'churn_rate_overall', 'churn_rate_more_avg']
+        })
+    
+    ax = churn_plot_df.plot(kind='bar', rot=0)
+
+    # Set the chart title and labels
+    ax.set_title(f'Churn Rate by segments of {column_name}', fontsize=16)
+    ax.set_xlabel('Segment', fontsize=14)
+    ax.set_ylabel('Churn Rate', fontsize=14)
+
+    # Set the x-axis tick labels
+    ax.set_xticklabels(['Below AVG', 'Overall', 'Above AVG'], fontsize=12)
+
+    # Show the chart
+    plt.show()
+
+
 
 def remove_constant_columns(df):
     """
@@ -85,7 +250,13 @@ def remove_null_rows(df):
 
 def flag_outliers(df):
     """
-    This function takes a pandas DataFrame as input and flags outliers in numerical columns.
+    Flags outliers and creates new numerical columns to flag them.
+
+    Args:
+    df: pandas DataFrame object.
+
+    Returns:
+    pandas DataFrame object with outliers clamped.
     """
     # Create a new DataFrame to store the outlier flags
     outlier_flags = pd.DataFrame(index=df.index)
@@ -120,11 +291,18 @@ def flag_outliers(df):
                 (df[col] < lower_bound) | (df[col] > upper_bound)
             ).astype(int)
 
+            print("Outlier column created:", col)
+            
+        else: print("No outlier columns created")
+
     # Join the outlier flags DataFrame with the input DataFrame
     df = pd.concat([df, outlier_flags], axis=1)
 
     # Return the new DataFrame with outlier flags
     return df
+
+    
+
 
 
 def check_for_duplicates(df):
